7f25c8fa66c68bc20d9aab2a766f7f39917f2991
Abd Alrhman <abdalrhman@Abds-MacBook-Pro.local>
2021-01-06 21:40:50 +0200

    lab 09

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -7,14 +7,14 @@ const supergoose = require('@code-fellows/supergoose');
 const mockRequest = supergoose(server);
 
 describe('Checking API Routes for both Products and Categories', ()=>{
-    let consoleSpy;
-    beforeEach(()=> {
-        consoleSpy = jest.spyOn(console, 'log').mockImplementation();
-    });
+    // let consoleSpy;
+    // beforeEach(()=> {
+    //     consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+    // });
 
-    afterEach(()=> {
-        consoleSpy.mockRestore();
-    });
+    // afterEach(()=> {
+    //     consoleSpy.mockRestore();
+    // });
 
     // Categories api server
     it('can post a category item', async()=> {
@@ -23,8 +23,7 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'awesome'
         };
-        const data = await mockRequest.post('/categories').send(categoryObj);
-        // console.log("data.body", data.body);
+        const data = await mockRequest.post('/api/v1/categories').send(categoryObj);
         
         const record = data.body;
         Object.keys(categoryObj).forEach(key=> {
@@ -38,10 +37,10 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'awesome'
         };
-        const data = await mockRequest.post('/categories').send(categoryObj);
+        const data = await mockRequest.post('/api/v1/categories').send(categoryObj);
         const record = data.body;
-        const categoryItemResponse =  await mockRequest.get(`/categories/${record._id}`) ;
-        const categoryItem = categoryItemResponse.body[0];
+        const categoryItemResponse =  await mockRequest.get(`/api/v1/categories/${record._id}`) ;
+        const categoryItem = categoryItemResponse.body.data[0];
         Object.keys(categoryObj).forEach(key=> {
             expect(categoryItem[key]).toEqual(categoryObj[key]);
         });
@@ -59,9 +58,9 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'awesome'
         };
-        const data = await mockRequest.post('/categories').send(categoryObj);
+        const data = await mockRequest.post('/api/v1/categories').send(categoryObj);
         const record = data.body;
-        const categoryItemResponse =  await mockRequest.put(`/categories/${record._id}`).send(categoryObjUpdate);
+        const categoryItemResponse =  await mockRequest.put(`/api/v1/categories/${record._id}`).send(categoryObjUpdate);
         const categoryItem = categoryItemResponse.body[0];
         Object.keys(categoryObjUpdate).forEach(key=> {
             expect(categoryItem[key]).toEqual(categoryObjUpdate[key]);
@@ -74,9 +73,9 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'awesome'
         };
-        const data = await mockRequest.post('/categories').send(categoryObj);
+        const data = await mockRequest.post('/api/v1/categories').send(categoryObj);
         const record = data.body;
-        const categoryItemResponse =  await mockRequest.delete(`/categories/${record._id}`);
+        const categoryItemResponse =  await mockRequest.delete(`/api/v1/categories/${record._id}`);
         const categoryItem = categoryItemResponse.body;
         Object.keys(categoryObj).forEach(key=> {
             expect(categoryItem[key]).toEqual(categoryObj[key]);
@@ -92,7 +91,7 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'awesome'
         };
-        const data = await mockRequest.post('/products').send(productObj);
+        const data = await mockRequest.post('/api/v1/products').send(productObj);
         // console.log("data.body", data.body);
         
         const record = data.body;
@@ -108,10 +107,10 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'awesome'
         };
-        const data = await mockRequest.post('/products').send(productObj);
+        const data = await mockRequest.post('/api/v1/products').send(productObj);
         const record = data.body;
-        const productItemResponse =  await mockRequest.get(`/products/${record._id}`) ;
-        const productItem = productItemResponse.body[0];
+        const productItemResponse =  await mockRequest.get(`/api/v1/products/${record._id}`) ;
+        const productItem = productItemResponse.body.data[0];
         Object.keys(productObj).forEach(key=> {
             expect(productItem[key]).toEqual(productObj[key]);
         });
@@ -131,9 +130,9 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'great!'
         };
-        const data = await mockRequest.post('/products').send(productObj);
+        const data = await mockRequest.post('/api/v1/products').send(productObj);
         const record = data.body;
-        const productItemResponse =  await mockRequest.put(`/products/${record._id}`).send(productObjUpdate);
+        const productItemResponse =  await mockRequest.put(`/api/v1/products/${record._id}`).send(productObjUpdate);
         const productItem = productItemResponse.body[0];
         Object.keys(productObjUpdate).forEach(key=> {
             expect(productItem[key]).toEqual(productObjUpdate[key]);
@@ -147,9 +146,9 @@ describe('Checking API Routes for both Products and Categories', ()=>{
             display_name: 'iOS',
             description: 'awesome'
         };
-        const data = await mockRequest.post('/products').send(productObj);
+        const data = await mockRequest.post('/api/v1/products').send(productObj);
         const record = data.body;
-        const productItemResponse =  await mockRequest.delete(`/products/${record._id}`);
+        const productItemResponse =  await mockRequest.delete(`/api/v1/products/${record._id}`);
         const productItem = productItemResponse.body;
         Object.keys(productObj).forEach(key=> {
             expect(productItem[key]).toEqual(productObj[key]);
---
+++ routes/api-v1.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const express = require('express');
+const products = require('../lib/models/products/products.collection')
+const categories = require('../lib/models/categories/categories.collection')
+
+const router = express.Router();
+
+router.post('/api/v1/:model', postHandler);
+router.get('/api/v1/:model', getHandler);
+router.get('/api/v1/:model/:id', getHandler);
+router.put('/api/v1/:model/:id', updateHandler);
+router.delete('/api/v1/:model/:id', deleteHandler);
+
+router.param('model', getModel);
+
+// categories routes
+function getHandler (req, res, next) {
+    let id = req.params.id;
+    req.model.read(id).then(data=>{
+        let count = data.length;
+        res.status(200).json({count, data});
+    })
+    .catch(next);
+}
+
+function postHandler (req, res, next){
+    req.model.create(req.body).then(data=>{
+        console.log("post handler ", data)
+        res.status(201).json(data);
+    })
+    .catch(next);
+}
+
+function updateHandler (req, res, next){
+    let id = req.params.id;
+    req.model.update(id, req.body).then(()=>{
+        req.model.read(id).then(data=>{
+            res.status(200).json(data);
+        })
+    })
+    .catch(next);
+}
+
+function deleteHandler (req, res, next){
+    let id = req.params.id;
+    req.model.delete(id).then(data=>{
+        res.status(200).json(data);
+    })
+    .catch(next);
+}
+
+function getModel (req, res, next) {
+    let model = req.params.model;
+    switch(model) {
+        case "products":
+            req.model = products;
+            next()
+            break;
+        case "categories":
+            req.model = categories;
+            next()
+            break;
+        default:
+            next('Invalid Route!! Please try again!')
+            break;
+    }
+}
+
+module.exports = router;
